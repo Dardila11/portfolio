d7a169e65c6983fc8259f5432bbcdf1fed303bd2
Daniel Ardila <danieleardila11@gmail.com>
2023-11-14 04:20:13 -0500

    about me section

---
+++ components/about.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { motion } from 'framer-motion';
+import SectionHeading from "./section-heading";
+
+export default function About() {
+  return (
+    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading> about me </SectionHeading>
+      <p className="mb-3">
+        Hello! I'm Daniel, a passionate software engineer with a focus on <span className="font-medium">iOS.</span>{" "}
+        I love creating cross-platform experiences using <span className="font-medium">Flutter</span>  and
+        diving into web development with <span className="font-medium">React and Next.js.</span>{" "}
+
+        Let's build something amazing together!
+      </p>
+    </motion.section>
+  )
+}
